097bf26bc3188ccd54d8ac90f75ac5905888eb70
zonble <zonble@gmail.com>
2018-01-19 12:35:42 +0800

    Adds documentation.

---
+++ jazzy.sh
@@ -0,0 +1,9 @@
+jazzy \
+  --author zonble \
+  --author_url https://zonble.net \
+  --github_url https://github.com/zonbel/ZBSimplePluginManager \
+  --github-file-prefix https://github.com/zonble/ZBSimplePluginManager/tree/0.0.1 \
+  --module-version 0.0.1 \
+  --xcodebuild-arguments -scheme,ZBSimplePluginManager \
+  --module ZBSimplePluginManager \
+  --module ZBSimplePluginManager
